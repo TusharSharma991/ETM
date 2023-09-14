dcfddf4d6869aae2844fd13d02b501893285378f
Tushar Sharma <tusharshts27601@gmail.com>
2023-09-14 12:08:35 +0530

    first

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='heading'>
+      <h2 className='heads'>
+      Expense Tracker
+      </h2>
+    </div>
+    
+  )
+}
